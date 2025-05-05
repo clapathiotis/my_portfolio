e7b188feff7a0b38bebb97c6994875931cb733e9
Christoforos <clapathiotis@gmail.com>
2025-05-05 17:56:50 +0200

    animation on about me

--- portfolio/src/app/components/about-me/about-me.component.ts
+++ portfolio/src/app/components/about-me/about-me.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  standalone: true,
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add this line
 })
-export class AboutMeComponent {
-
-}
+export class AboutMeComponent {}
