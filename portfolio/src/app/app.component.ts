f60766df153276106034da17e234f8ec0e163fb2
Christoforos <clapathiotis@gmail.com>
2025-05-05 16:12:56 +0200

    about and arrow addition

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, ProfileComponent, FooterComponent, PortfolioComponent],
+  imports: [CommonModule, HeaderComponent, ProfileComponent, FooterComponent, PortfolioComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
