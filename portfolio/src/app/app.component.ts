85b7e9bc86659612cb43d6e8de905954bb0d4581
Christoforos <clapathiotis@gmail.com>
2025-05-05 14:58:15 +0200

    components & assets

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, PortfolioComponent],
+  imports: [HeaderComponent, ProfileComponent, FooterComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
