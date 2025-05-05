dc67cb16e37efcb41e6face90bf150838cdf5f83
Christoforos <clapathiotis@gmail.com>
2025-05-05 18:21:57 +0200

    redo docs

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
+// import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, ProfileComponent, FooterComponent, PortfolioComponent, AboutMeComponent],
+  imports: [CommonModule, HeaderComponent, ProfileComponent, FooterComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
