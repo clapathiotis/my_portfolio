4d184fcdebd3a76281884638b3cd25515476f0cc
Christoforos <clapathiotis@gmail.com>
2025-06-07 17:49:51 +0200

    header animations for mobile

--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -1,38 +1,62 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   animations: [
-    trigger('dropdownAnimation', [
-      state('closed', style({
-        opacity: 0,
-        transform: 'translateY(-10px)',
-        maxHeight: '0',
-        overflow: 'hidden'
-      })),
-      state('open', style({
-        opacity: 1,
-        transform: 'translateY(0)',
-        display: 'block'
-      })),
-      transition('closed => open', [
-        style({ display: 'block' }), // Ensure the element is displayed before animating
-        animate('300ms ease-in-out')
+    trigger('dropdownItems', [
+      transition(':enter', [
+        query(
+          '.header__link',
+          [
+            style({ opacity: 0, transform: 'translateY(-20px)' }),
+            stagger('100ms', [
+              animate(
+                '300ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
       ]),
-      transition('open => closed', [
-        animate('300ms ease-in-out', style({ opacity: 0, transform: 'translateY(-10px)' })),
-        style({ display: 'none' }) // Hide the element after the animation
-      ])
-    ])
-  ]
+      transition(':leave', [
+        query(
+          '.header__link',
+          [
+            stagger('100ms', [
+              animate(
+                '300ms ease-in',
+                style({ opacity: 0, transform: 'translateY(-20px)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HeaderComponent {
   menuOpen = false;
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
   }
 }
