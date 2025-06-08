37f1934b277b547b8933f738ebb20d1a83f789a1
Christoforos <clapathiotis@gmail.com>
2025-06-08 17:06:00 +0200

    optimized code for animations of profile

--- portfolio/src/app/components/profile/profile.component.ts
+++ portfolio/src/app/components/profile/profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, AfterViewInit } from '@angular/core';
-import { trigger, style, transition, animate, keyframes, state } from '@angular/animations';
 import { FloatingArrowComponent } from '../shared/floating-arrow/floating-arrow.component';
-
+import { slideInRightAnimation, slideInLeftAnimation, slideInUpAnimation, fadeInSocialsAnimation } from '../shared/animations/animations';
 @Component({
   selector: 'app-profile',
   standalone: true,
@@ -9,51 +8,6 @@ import { FloatingArrowComponent } from '../shared/floating-arrow/floating-arrow.
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  animations: [
-    trigger('slideInRight', [
-      transition(':enter', [
-        animate(
-          '2s ease-out',
-          keyframes([
-            style({ opacity: 0, transform: 'translateX(100px)', offset: 0 }),
-            style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
-          ])
-        )
-      ])
-    ]),
-    trigger('slideInLeft', [
-      transition(':enter', [
-        animate(
-          '2s ease-out',
-          keyframes([
-            style({ opacity: 0, transform: 'translateX(-100px)', offset: 0 }),
-            style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
-          ])
-        )
-      ])
-    ]),
-    trigger('slideInUp', [
-      transition(':enter', [
-        animate(
-          '2s ease-out',
-          keyframes([
-            style({ opacity: 0, transform: 'translateY(50px)', offset: 0 }),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
-          ])
-        )
-      ])
-    ]),
-    trigger('fadeInSocials', [
-      transition(':enter', [
-        animate(
-          '5s ease-out',
-          keyframes([
-            style({ opacity: 0, transform: 'translateY(20px)', offset: 0 }),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
-          ])
-        )
-      ])
-    ])
-  ]
+  animations: [slideInLeftAnimation, slideInRightAnimation, slideInUpAnimation, fadeInSocialsAnimation]
 })
 export class ProfileComponent {}
--- portfolio/src/app/components/shared/animations/animations.ts
+++ portfolio/src/app/components/shared/animations/animations.ts
@@ -1,7 +1,55 @@
-import { trigger, transition, style, animate, state } from '@angular/animations';
+import { trigger, transition, style, animate, state, keyframes} from '@angular/animations';
 
 export const fadeInUpAnimation = trigger('fadeInUp', [
   state('false', style({ opacity: 0, transform: 'translateY(40px)' })), // Initial state
   state('true', style({ opacity: 1, transform: 'translateY(0)' })), // Final state
   transition('false => true', animate('1500ms ease-out')), // Animation when transitioning to visible
+]);
+
+export const slideInRightAnimation = trigger('slideInRight', [
+  transition(':enter', [
+    animate(
+      '2s ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'translateX(100px)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
+      ])
+    )
+  ])
+]);
+
+export const slideInLeftAnimation = trigger('slideInLeft', [
+  transition(':enter', [
+    animate(
+      '2s ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'translateX(-100px)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
+      ])
+    )
+  ])
+]);
+
+export const slideInUpAnimation = trigger('slideInUp', [
+  transition(':enter', [
+    animate(
+      '2s ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'translateY(50px)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+      ])
+    )
+  ])
+]);
+
+export const fadeInSocialsAnimation = trigger('fadeInSocials', [
+  transition(':enter', [
+    animate(
+      '5s ease-out',
+      keyframes([
+        style({ opacity: 0, transform: 'translateY(20px)', offset: 0 }),
+        style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+      ])
+    )
+  ])
 ]);
