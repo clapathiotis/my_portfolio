6d11bdfd7b5e5ea533830ec6228fa15ccec45785
Christoforos <clapathiotis@gmail.com>
2025-07-16 16:20:18 +0200

    footer update

--- portfolio/src/app/components/footer/footer.component.ts
+++ portfolio/src/app/components/footer/footer.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { IntersectionObserverDirective } from '../shared/animations/intersection-observer.directive';
+import { fadeInUpAnimation } from '../shared/animations/animations';
 
 @Component({
   selector: 'app-footer',
+  imports: [IntersectionObserverDirective],
+  animations: [fadeInUpAnimation],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  isVisible = false; // Track visibility state
+
+  onVisibilityChange(visible: boolean): void {
+    this.isVisible = visible; // Update visibility state
+  }
+}
